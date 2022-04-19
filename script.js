2abe1eb996719388ccda9765dbcf923f3d95c3c2
Bendiz <benerok@icloud.com>
2022-04-19 22:28:56 +0200

    Simplify code and target button with JS

--- script.js
+++ script.js
@@ -1,49 +1,14 @@
-function computerPlay(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(3);
+function computerPlay() {
     let value = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    switch (value) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors;"
+    }
+computerPlay();
 }
 
-computerPlay()
-    if (value === 1) {
-    return ("Rock")
-
-}   else if (value === 2) {
-    return ("Paper")
-
-}   else if (value === 3) {
-    return ("Scissors")
-}
-
-// Two parameters (playerSelection, computerSelection) //
-// Return "You win! Paper beats Scissors" OR "You Lose! Paper beats Rock" //
-// playerSelection case-insensitive //
-
-// Create input option type = text //
-
-// Rock = 1
-// Paper = 2
-// Scissors = 3
-
-// Rock beats Scissors //
-// Paper beats Rock //
-// Scissors beats Paper //
-
-// if (playerSelection == "Rock" && computerSelection == "Scissors") {
-//   return "You Win! Rock beats Scissors"; }
-// }else if (playerSelection === "Rock" && computerSelection === "Paper") {
-//      return "You Lose! Paper beats Rock"; }
-//}else if (playerSelection === "Rock" && computerSelection === "Rock") {
-//      return "Tie!"; }
-// }else if (playerSelection === "Paper" && computerSelection === "Rock") {
- //        return "You Win! Paper beats Rock";}
- // }else if (playerSelection === "Paper" && computerSelection === "Scissors") {
- //        return "You Lose! Scissors beats Paper";}
- //}else if (playerSelection === "Paper" && computerSelection === "Paper") {
-//      return "Tie!"; }
-// }else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-//          return "You Win! Scissors beats Paper";}
-// }else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-//          return "You Lose! Rock beats Scissors";}
- //}else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-//      return "Tie!"; }
+document.getElementById("button")
